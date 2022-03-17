d3463fa78c17848cc7e9fb19270713e8c2f5dbef
iRekila <93725832+iRekila@users.noreply.github.com>
2022-03-17 13:41:43 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const URL = 'http://localhost/shoppinglist/';
@@ -7,88 +7,78 @@ const URL = 'http://localhost/shoppinglist/';
 
 
 function App() {
- const [items, setItems] = useState([]);
- const [item, setItem] = useState('');
- const [amount, setAmount] = useState('');
+  const [items, setItems] = useState([]);
+  const [item, setItem] = useState('');
+  const [amount, setAmount] = useState('');
 
 
- useEffect(() => {
-  axios.get(URL)
-  .then((response) => {
-    setItems(response.data)
-  }).catch(error => {
-    alert(error.response ? error.response.data.error : error);
-  });
-  
-}, [])
+  useEffect(() => {
+    axios.get(URL)
+      .then((response) => {
+        setItems(response.data)
+      }).catch(error => {
+        alert(error.response ? error.response.data.error : error);
+      });
 
-function save(e) {
-  e.preventDefault();
-  const json = JSON.stringify({description:item,amount:amount})
-  axios.post(URL + 'add.php',json, {
+  }, [])
+
+  function save(e) {
+    e.preventDefault();
+    const json = JSON.stringify({ description: item, amount: amount })
+    axios.post(URL + 'add.php', json, {
       headers: {
-        'Content-Type':'application/json'
+        'Content-Type': 'application/json'
       }
-  })
+    })
 
 
-  .then((response) => {
-    setItems(items => [...items,response.data]);
-    setItem('');
-    setAmount('');
-  }).catch (error => {
-    alert(error.response.data.error)
-  })
-}
+      .then((response) => {
+        setItems(items => [...items, response.data]);
+        setItem('');
+        setAmount('');
+      }).catch(error => {
+        alert(error.response.data.error)
+      })
+  }
 
-function remove(id) {
-  const json = JSON.stringify({id:id})
-  axios.post(URL + 'delete.php',json,{
-    headers: {
-      'Content-Type' : 'application/json'
-    }
-  })
-  .then((response)=> {
-    const newListWithoutRemoved = items.filter((item) => item.id !== id);
-    setItems(newListWithoutRemoved);
-  }).catch (error => {
-    alert(error.response ? error.response.data.error : error);
-  })
-}
+  function remove(id) {
+    const json = JSON.stringify({ id: id })
+    axios.post(URL + 'delete.php', json, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((response) => {
+        const newListWithoutRemoved = items.filter((item) => item.id !== id);
+        setItems(newListWithoutRemoved);
+      }).catch(error => {
+        alert(error.response ? error.response.data.error : error);
+      })
+  }
 
 
 
   return (
     <div className="container">
-    <h3>Shopping list</h3>
-    <form onSubmit={save}>
-      <label>New item </label>
-      <input placeholder="type item" value={item} onChange={e => setItem(e.target.value)}/>
-      <input placeholder="type amount" value={amount} onChange={e => setAmount(e.target.value)}/>
-      <button>Add</button>
-    </form>
-<ul>
-
-         {items?.map(item => (
-         <a class="item" key={item.id}>
-         {item.id &&
-         item.description
-         }&nbsp;
-                     
-        <a class="amount" key={item.id}>
-          {item.id &&
-          item.amount
-          }&nbsp;
+      <h3>Shopping list</h3>
+      <form onSubmit={save}>
+        <label>New item </label>
+        <input placeholder="type item" value={item} onChange={e => setItem(e.target.value)} />
+        <input placeholder="type amount" value={amount} onChange={e => setAmount(e.target.value)} />
+        <button>Add</button>
+      </form>
+      <ul>
+        {items?.map(item => (
+          <a class="item" key={item.id}>
+            {item.id && item.description}&nbsp;
+            <a class="amount" key={item.id}>
+              {item.id && item.amount}&nbsp; </a>
+            <a className="delete" onClick={() => remove(item.id)} href="#">
+              Delete items
+            </a>
+            <br></br>
           </a>
-          <a className="delete" onClick={() => remove(item.id)} href="#">
-          Delete items
-          </a>
-          <br></br>
-          </a>
-
-          
-         
-      ))} 
+        ))}
       </ul>
     </div>
   );
@@ -97,6 +87,4 @@ function remove(id) {
 
 
 export default App;
-
-
 
